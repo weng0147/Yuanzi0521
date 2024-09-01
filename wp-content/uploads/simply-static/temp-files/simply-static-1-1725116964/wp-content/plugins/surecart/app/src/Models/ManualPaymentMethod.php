07a3ae5764e3ba50092f8bf4cb5f5d94accec657
weng0147 <fallenfrost0521@gmail.com>
2024-09-01 11:31:18 +0800

    works

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1725116964/wp-content/plugins/surecart/app/src/Models/ManualPaymentMethod.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;SureCart\Models\Model&quot; not found in E:\xampp\htdocs\testsite\wordpress-6.6.1-zh_CN\wordpress\wp-content\plugins\surecart\app\src\Models\ManualPaymentMethod.php:7
+Stack trace:
+#0 {main}
+  thrown in <b>E:\xampp\htdocs\testsite\wordpress-6.6.1-zh_CN\wordpress\wp-content\plugins\surecart\app\src\Models\ManualPaymentMethod.php</b> on line <b>7</b><br>
---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1725116964/wp-content/plugins/surecart/app/src/Models/UpsellFunnel.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;SureCart\Models\Model&quot; not found in E:\xampp\htdocs\testsite\wordpress-6.6.1-zh_CN\wordpress\wp-content\plugins\surecart\app\src\Models\UpsellFunnel.php:8
+Stack trace:
+#0 {main}
+  thrown in <b>E:\xampp\htdocs\testsite\wordpress-6.6.1-zh_CN\wordpress\wp-content\plugins\surecart\app\src\Models\UpsellFunnel.php</b> on line <b>8</b><br>
